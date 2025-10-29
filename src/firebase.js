f953fdd47c31fbbede015ba78662f4438be62813
Franz <franzkingstein@gmail.com>
2025-10-29 19:50:14 +0530

    Corrected the Environment Variables

--- src/firebase.js
+++ src/firebase.js
@@ -6,12 +6,12 @@ import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCcwvPLdKoK3MJF2nCfe3kcX5cTc5pYXC4",
-  authDomain: "pace-cd877.firebaseapp.com",
-  projectId: "pace-cd877",
-  storageBucket: "pace-cd877.firebasestorage.app",
-  messagingSenderId: "891538602468",
-  appId: "1:891538602468:web:55e758135152510950eff1"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY || "AIzaSyCcwvPLdKoK3MJF2nCfe3kcX5cTc5pYXC4",
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN || "pace-cd877.firebaseapp.com",
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID || "pace-cd877",
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET || "pace-cd877.firebasestorage.app",
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID || "891538602468",
+  appId: process.env.REACT_APP_FIREBASE_APP_ID || "1:891538602468:web:55e758135152510950eff1"
 };
 
 // Initialize Firebase
